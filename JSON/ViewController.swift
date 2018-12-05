f526253d253251b0dd9a2e9372e6319e9d11ace9
BSH_MRM <bsh_mrm@bsh-mrm-macbook-pro.local>
2018-12-05 15:20:32 +0300

    Initial Commit

---
+++ JSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSON
+//
+//  Created by BSH_MRM on 5.12.2018.
+//  Copyright © 2018 BSH_MRM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
